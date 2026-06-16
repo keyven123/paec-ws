4d643d508f45cc9c29cebd81e6e6e0a6823e21fa
kmrosal <keyvenrosal14@gmail.com>
2026-06-16 23:55:17 +0800

    fix permission seeder

--- database/seeders/PaecAdminUserSeeder.php
+++ database/seeders/PaecAdminUserSeeder.php
@@ -9,26 +9,26 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class SuperAdminUserSeeder extends Seeder
+class PaecAdminUserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //to run this seeder php artisan db:seed --class=AdminUserSeeder
+        //to run this seeder php artisan db:seed --class=PaecAdminUserSeeder
 
         DB::beginTransaction();
-        $role = Role::whereCode(GeneralConstants::ROLES['SUPER_ADMIN']['name'])->first();
+        $role = Role::whereCode(GeneralConstants::ROLES['ORGANIZER']['name'])->first();
 
         if ($role) {
             AdminUser::updateOrCreate(
-                ['email' => 'admin@ticketoc.com'],
+                ['email' => 'admin@paec.com'],
                 [
                     'role_uuid' => $role->uuid,
-                    'password' => '123ticketoc$$$',
+                    'password' => 'P@ec2026!!',
                     'email_verified_at' => now(),
-                    'first_name' => 'Super',
+                    'first_name' => 'PAEC',
                     'last_name' => 'Admin',
                     'is_first_time_login' => false,
                     'email_verified_at' => now(),
